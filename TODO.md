1f32b9601c40cbee920fe9b25375118345899c8f
JanPanjan <89231282@student.upr.si>
2025-06-14 23:51:14 +0200

    todo

---
+++ TODO.md
@@ -0,0 +1,8 @@
+- [ ] sinhronizirat barve med status bar, neovim in terminalom
+    - quiet dark
+    - ugotovit kaj naredit za light theme
+- [ ] ugotovit kako zamenjat GTK theme
+- [ ] script da mi zamenja theme
+    - <https://github.com/stdrice/pengurice/blob/main/dotfiles/.local/bin/theme-changer>
+
+- [x] naredi custom wallpaper za swayidle tako da pise "odkleni me" al nekaj
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,7 +9,7 @@ end
 
 local function scheme_for_appearance(appearance)
 	-- if appearance:find 'Dark' then
-	return 'OneDark (Gogh)'
+	-- return 'OneDark (Gogh)'
 	-- else
 	-- return 'One Light (Gogh)'
 	-- return 'Ayu Light (Gogh)'
@@ -20,7 +20,7 @@ local config = wezterm.config_builder()
 
 config.initial_cols = 120
 config.initial_rows = 28
-config.font_size = 13
+config.font_size = 14
 
 config.color_scheme = scheme_for_appearance(get_appearance())
 
